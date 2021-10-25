4c552557f222b77998ea884e1a5384c7ae7f6ac8
saitejahara <saiteja.hara@zemosolabs.com>
2021-10-25 21:48:56 +0530

    add auth changes

---
+++ src/errors/Messages.js
@@ -0,0 +1,8 @@
+export const clientIDNotFound = "clientID not found";
+export const tokenURINotFound = "tokenURI not found";
+export const keyIDNotFound = "keyID not found";
+export const expiryTimeNotFound = "expiryTime not found";
+export const privateKeyNotFound = "privateKey not found";
+export const credentialsObjCannotBeProcessed =
+  "credentialsObj cannot be processed";
+export const notAValidJSON = "not a valid JSON object";
---
+++ src/service-account/util/Token.js
@@ -0,0 +1,84 @@
+import fs from "fs";
+import Axios from "axios";
+import { logger } from "logger";
+import jwt from "jsonwebtoken";
+import * as messageVar from "../../errors/Messages.js";
+
+function IsJsonString(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function GenerateToken(credentialsFilePath) {
+  try {
+    const credentials = fs.readFileSync(credentialsFilePath, "utf8");
+
+    if (typeof JSON.parse(credentials) !== "object") {
+      throw new Error(messageVar.notAValidJSON);
+    }
+
+    if (!IsJsonString(credentials)) {
+      throw new Error(messageVar.notAValidJSON);
+    }
+
+    const credentialsObj = JSON.parse(credentials);
+
+    const expiryTime = Math.floor(Date.now() / 1000) + 60;
+    const claims = {
+      iss: credentialsObj.clientID,
+      key: credentialsObj.keyID,
+      aud: credentialsObj.tokenURI,
+      exp: expiryTime,
+      sub: credentialsObj.clientID,
+    };
+
+    if (claims.iss == null) {
+      throw new Error(messageVar.clientIDNotFound);
+    }
+    if (claims.key == null) {
+      throw new Error(messageVar.keyIDNotFound);
+    }
+    if (claims.aud == null) {
+      throw new Error(messageVar.tokenURINotFound);
+    }
+    if (expiryTime == null) {
+      throw new Error(messageVar.expiryTimeNotFound);
+    }
+    if (credentialsObj.privateKey == null) {
+      throw new Error(messageVar.privateKeyNotFound);
+    }
+
+    const privateKey = credentialsObj.privateKey.toString("utf8");
+
+    const signedJwt = jwt.sign(claims, privateKey, { algorithm: "RS256" });
+    return new Promise((resolve, reject) => {
+      Axios(`${credentialsObj.tokenURI}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        data: {
+          grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+          assertion: signedJwt,
+        },
+      })
+        .then((res) => {
+          resolve({
+            accessToken: res.data.accessToken,
+            tokenType: res.data.tokenType,
+          });
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }).catch((err) => {
+      logger.error(err.toString());
+    });
+  } catch (err) {
+    logger.error(err.toString());
+  }
+}
+
+export default GenerateToken;
