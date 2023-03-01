c9a894cd1bdb96a6d7748dd8723add8b2f3f93cd
Shaik-Luqmaan <luqmaan.shaik@skyflow.com>
2023-03-01 14:25:23 +0530

    SK-529/ Fix grace period of token caching
    Signed-off-by: Shaik-Luqmaan <luqmaan.shaik@skyflow.com>

--- src/vault-api/utils/jwt-utils/index.ts
+++ src/vault-api/utils/jwt-utils/index.ts
@@ -31,7 +31,7 @@ function isTokenValid(token: string) {
   if(token === "") return false
   let isJwtExpired = false;
   const decoded: JwtPayload = jwt_decode(token);
-  const currentTime = (new Date().getTime() / 1000) - 300;
+  const currentTime = (new Date().getTime() / 1000) + 300;
   const expiryTime = decoded.exp;
   if (expiryTime && currentTime > expiryTime) {
     isJwtExpired = true;
