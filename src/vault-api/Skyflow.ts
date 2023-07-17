5fbe128a28d8cc69a054964da30a1ee307cefe6c
ShivkeshMadasu <shivakesh026@gmail.com>
2023-07-17 17:25:02 +0530

    DEX-229 SDK references

--- src/vault-api/Skyflow.ts
+++ src/vault-api/Skyflow.ts
@@ -2,104 +2,183 @@
 	Copyright (c) 2022 Skyflow, Inc. 
 */
 
-import Client from './client';
-import { printLog } from './utils/logs-helper';
-import logs from './utils/logs';
-import Controller from './Controller';
-import {
-  IRevealResponseType,
-  IConnectionConfig,
-  RequestMethod,
-  IInsertRecordInput,
-  IDetokenizeInput,
-  IGetByIdInput,
-  RedactionType,
-  MessageType,
-  IInsertOptions,
-  IUpdateInput,
-  IUpdateOptions,
-  IGetInput,
-} from './utils/common';
-import { formatVaultURL } from './utils/helpers';
+/**
+ * This is the doc comment for Skyflow Module
+ * @module Skyflow
+ */
 
-export interface ISkyflow {
-  vaultID?: string;
-  vaultURL?: string;
-  getBearerToken: () => Promise<string>;
-  options?: Record<string, any>;
-}
+ import Client from './client';
+ import { printLog } from './utils/logs-helper';
+ import logs from './utils/logs';
+ import Controller from './Controller';
+ import {
+   IRevealResponseType,
+   IConnectionConfig,
+   RequestMethod,
+   IInsertRecordInput,
+   IDetokenizeInput,
+   IGetByIdInput,
+   RedactionType,
+   MessageType,
+   IInsertOptions,
+   IUpdateInput,
+   IUpdateOptions,
+   IGetInput,
+ } from './utils/common';
+ import { formatVaultURL } from './utils/helpers';
+ 
 
-class Skyflow {
-  #client: Client;
-  #metadata = { };
-  #Controller: Controller;
+ /**
+ * This is documentation for interface ISkyflow.
+ * @property vaultID This is the vaultID property
+ * @property vaultURL This is the vaultURL property
+ * @property getBearerToken This is the getBearerToken property
+ * @property options This is the options property
+ */
+ export interface ISkyflow {
+   vaultID?: string;
+   vaultURL?: string;
+   getBearerToken: () => Promise<string>;
+   options?: Record<string, any>;
+ }
+ 
+ /**
+  * This is the documentation for Skyflow Class
+  * @class Skyflow
+  */
+ class Skyflow {
+   /**
+    * @internal
+    */
+   #client: Client;
+ 
+   /**
+    * @internal
+    */
+   #metadata = { };
+ 
+   /**
+    * @internal
+    */
+   #Controller: Controller;
+ 
+  /**
+  * Some documentation for constructor
+  * @param config This is a description of the first parameter.
+  */
+   constructor(config: ISkyflow) {
+     this.#client = new Client(
+       {
+         ...config,
+       }
+       ,
+       this.#metadata,
+     );
+     this.#Controller = new Controller(this.#client);
+ 
+     printLog(logs.infoLogs.BEARER_TOKEN_LISTENER, MessageType.LOG);
+   }
+ 
+ 
+  /**
+  * Some documentation for init method
+  * @public
+  * @param config This is a description of the first parameter.
+  * @returns This is a description of what the method returns.
+  */
+   static init(config: ISkyflow): Skyflow {
+     printLog(logs.infoLogs.INITIALIZE_CLIENT, MessageType.LOG);
+     config.vaultURL = formatVaultURL(config.vaultURL)
+     const skyflow = new Skyflow(config);
+     printLog(logs.infoLogs.CLIENT_INITIALIZED, MessageType.LOG);
+     return skyflow;
+   }
+ 
+  /**
+  * Some documentation for insert method
+  * @public
+  * @param records This is a description of the first parameter.
+  * @param options This is a description of the second parameter.
+  * @returns This is a description of what the method returns.
+  */
+   insert(
+     records: IInsertRecordInput,
+     options?: IInsertOptions,
+   ) {
+     printLog(logs.infoLogs.INSERT_TRIGGERED, MessageType.LOG);
+     return this.#Controller.insert(records, options);
+   }
+ 
+  /**
+  * Some documentation for detokenize method
+  * @public
+  * @param detokenizeInput This is a description of the first parameter.
+  * @returns This is a description of what the method returns.
+  */
+   detokenize(detokenizeInput: IDetokenizeInput): Promise<IRevealResponseType> {
+     printLog(logs.infoLogs.DETOKENIZE_TRIGGERED,
+       MessageType.LOG);
+     return this.#Controller.detokenize(detokenizeInput);
+   }
+ 
+  /**
+  * Some documentation for getById method
+  * @public
+  * @deprecated Use {@link get} instead.
+  * @param getByIdInput This is a description of the first parameter.
+  * @returns This is a description of what the method returns.
+  */
+   getById(getByIdInput: IGetByIdInput) {
+     printLog(logs.infoLogs.GET_BY_ID_TRIGGERED,
+       MessageType.LOG);
+     return this.#Controller.getById(getByIdInput);
+   }
+ 
+  /**
+  * Some documentation for get method
+  * @public
+  * @param getInput This is a description of the first parameter.
+  * @returns This is a description of what the method returns.
+  */
+   get(getInput: IGetInput) {
+     printLog(logs.infoLogs.GET_CALL_TRIGGERED,
+       MessageType.LOG);
+     return this.#Controller.get(getInput);
+   }
+ 
+  /**
+  * Some documentation for invokeConnection method
+  * @public
+  * @param config This is a description of the first parameter.
+  * @returns This is a description of what the method returns.
+  */
+   invokeConnection(config: IConnectionConfig) {
+     printLog(logs.infoLogs.INVOKE_CONNECTION_TRIGGERED,
+       MessageType.LOG);
+ 
+     return this.#Controller.invokeConnection(config);
+   }
+ 
+  /**
+  * Some documentation for update method
+  * @public
+  * @param updateInput This is a description of the first parameter.
+  * @param options This is a description of the second parameter.
+  * @returns This is a description of what the method returns.
+  */
+   update(updateInput: IUpdateInput,options?:IUpdateOptions){
+     printLog(logs.infoLogs.UPDATE_TRIGGERED,
+       MessageType.LOG);
+     return this.#Controller.update(updateInput,options);
+   }
+ 
+   static get RedactionType() {
+     return RedactionType;
+   }
 
-  constructor(config: ISkyflow) {
-    this.#client = new Client(
-      {
-        ...config,
-      }
-      ,
-      this.#metadata,
-    );
-    this.#Controller = new Controller(this.#client);
-
-    printLog(logs.infoLogs.BEARER_TOKEN_LISTENER, MessageType.LOG);
-  }
-
-  static init(config: ISkyflow): Skyflow {
-    printLog(logs.infoLogs.INITIALIZE_CLIENT, MessageType.LOG);
-    config.vaultURL = formatVaultURL(config.vaultURL)
-    const skyflow = new Skyflow(config);
-    printLog(logs.infoLogs.CLIENT_INITIALIZED, MessageType.LOG);
-    return skyflow;
-  }
-
-  insert(
-    records: IInsertRecordInput,
-    options?: IInsertOptions,
-  ) {
-    printLog(logs.infoLogs.INSERT_TRIGGERED, MessageType.LOG);
-    return this.#Controller.insert(records, options);
-  }
-
-  detokenize(detokenizeInput: IDetokenizeInput): Promise<IRevealResponseType> {
-    printLog(logs.infoLogs.DETOKENIZE_TRIGGERED,
-      MessageType.LOG);
-    return this.#Controller.detokenize(detokenizeInput);
-  }
-
-  getById(getByIdInput: IGetByIdInput) {
-    printLog(logs.infoLogs.GET_BY_ID_TRIGGERED,
-      MessageType.LOG);
-    return this.#Controller.getById(getByIdInput);
-  }
-
-  get(getInput: IGetInput) {
-    printLog(logs.infoLogs.GET_CALL_TRIGGERED,
-      MessageType.LOG);
-    return this.#Controller.get(getInput);
-  }
-
-  invokeConnection(config: IConnectionConfig) {
-    printLog(logs.infoLogs.INVOKE_CONNECTION_TRIGGERED,
-      MessageType.LOG);
-
-    return this.#Controller.invokeConnection(config);
-  }
-
-  update(updateInput: IUpdateInput,options?:IUpdateOptions){
-    printLog(logs.infoLogs.UPDATE_TRIGGERED,
-      MessageType.LOG);
-    return this.#Controller.update(updateInput,options);
-  }
-
-  static get RedactionType() {
-    return RedactionType;
-  }
-
-  static get RequestMethod() {
-    return RequestMethod;
-  }
-}
-export default Skyflow;
+   static get RequestMethod() {
+     return RequestMethod;
+   }
+ }
+ export default Skyflow;
+ 
--- src/vault-api/utils/common/index.ts
+++ src/vault-api/utils/common/index.ts
@@ -1,13 +1,25 @@
 /*
   Copyright (c) 2022 Skyflow, Inc. 
 */
-export enum RedactionType {
+
+/**
+ * This is the doc comment for Utils Module
+ * @module Utils
+ */
+
+/**
+ * This is documentation for RedactionType enumeration.
+ */
+ export enum RedactionType {
   DEFAULT = 'DEFAULT',
   PLAIN_TEXT = 'PLAIN_TEXT',
   MASKED = 'MASKED',
   REDACTED = 'REDACTED',
 }
 
+/**
+ * This is documentation for RequestMethod enumeration.
+ */
 export enum RequestMethod {
   GET = 'GET',
   POST = 'POST',
@@ -16,6 +28,9 @@ export enum RequestMethod {
   DELETE = 'DELETE',
 }
 
+/**
+ * This is documentation for LogLevel enumeration.
+ */
 export enum LogLevel {
   WARN = 'WARN',
   INFO = 'INFO',
@@ -24,36 +39,69 @@ export enum LogLevel {
   OFF = 'OFF'
 }
 
-
+/**
+ * This is documentation for MessageType enumeration.
+ */
 export enum MessageType {
   LOG = 'LOG',
   WARN = 'WARN',
   ERROR = 'ERROR',
 }
 
+/**
+ * This is documentation for interface IInsertRecordInput.
+ *  @property records This is the description for the records property
+ */
 export interface IInsertRecordInput {
   records: IInsertRecord[];
 }
 
+/**
+ * This is documentation for interface IInsertRecord.
+ *  @property table This is the description for the table property
+ *  @property fields This is the description for the fields property
+ */
 export interface IInsertRecord {
   table: string;
   fields: Record<string, any>;
 }
 
+/**
+ * This is documentation for interface IRevealRecord.
+ *  @property redaction This is the description for the redaction property
+ *  @property token This is the description for the token property
+ */
 export interface IRevealRecord {
   token: string;
   redaction?: RedactionType;
 }
 
+/**
+ * This is documentation for interface IRevealResponseType.
+ *  @property records This is the description for the records property
+ *  @property errors This is the description for the errors property
+ */
 export interface IRevealResponseType {
   records?: Record<string, string>[];
   errors?: Record<string, any>[];
 }
 
+/**
+ * This is documentation for interface IDetokenizeInput.
+ * @property records This is the description for the records property
+ */
 export interface IDetokenizeInput {
   records: IRevealRecord[];
 }
 
+/**
+ * This is documentation for interface ISkyflowIdRecord.
+ *  @property ids This is the description for the ids property
+ *  @property redaction This is the description for the redaction property
+ *  @property table This is the description for the table property
+ *  @property columnName This is the description for the columnName property
+ *  @property columnValues This is the description for the columnValues property
+ */
 export interface ISkyflowIdRecord {
   ids?: string[];
   redaction: RedactionType;
@@ -62,16 +110,30 @@ export interface ISkyflowIdRecord {
   columnValues?: string[];
 }
 
+/**
+ * This is documentation for interface ISkyflowRecord.
+ *  @property ids This is the description for the ids property
+ *  @property redaction This is the description for the redaction property
+ *  @property table This is the description for the table property
+ */
 export interface ISkyflowRecord {
   ids: string[];
   redaction: RedactionType;
   table: string;
 }
 
+/**
+ * This is documentation for interface IGetByIdInput.
+ *  @property records This is the description for the records property
+ */
 export interface IGetByIdInput {
   records: ISkyflowRecord[];
 }
 
+/**
+ * This is documentation for interface IGetInput.
+ * @property records This is the description for the records property
+ */
 export interface IGetInput {
   records: ISkyflowIdRecord[];
 }
@@ -80,6 +142,15 @@ export interface IGetInput {
 //   logLevel:LogLevel
 // }
 
+/**
+ * This is documentation for interface IConnectionConfig.
+ *  @property connectionURL This is the description for the connectionURL property
+ *  @property methodName This is the description for the methodName property
+ *  @property pathParams This is the description for the pathParams property
+ *  @property queryParams This is the description for the queryParams property
+ *  @property requestBody This is the description for the requestBody property
+ *  @property requestHeader This is the description for the requestHeader property
+ */
 export interface IConnectionConfig {
   connectionURL: string;
   methodName: RequestMethod;
@@ -97,6 +168,9 @@ export const TYPES = {
   INVOKE_CONNECTION: 'INVOKE_CONNECTION',
 };
 
+/**
+ * This is documentation for ContentType enumeration.
+ */
 export enum ContentType {
   APPLICATIONORJSON = 'application/json',
   TEXTORPLAIN = 'text/plain',
@@ -105,25 +179,50 @@ export enum ContentType {
   FORMDATA = 'multipart/form-data',
 }
 
+/**
+ * This is documentation for interface IUpsertOption.
+ *  @property table This is the description for the table property
+ *  @property column This is the description for the column property
+ */
 export interface IUpsertOption {
   table: string;
   column: string;
 }
 
+/**
+ * This is documentation for interface IInsertOptions.
+ * @property tokens This is the description for the tokens property
+ * @property upsert This is the description for the upsert property
+ */
 export interface IInsertOptions {
   tokens?: boolean;
   upsert?: IUpsertOption[];
 }
 
+/**
+ * This is documentation for interface IUpdateRecord.
+ *  @property id This is the description for the id property
+ *  @property table This is the description for the table property
+ *  @property fields This is the description for the fields property
+ */
 export interface IUpdateRecord{
   id: string,
   table: string,
   fields: Record<string,any>
 }
+
+/**
+ * This is documentation for interface IUpdateInput.
+ *  @property records This is the description for the records property
+ */
 export interface IUpdateInput{
   records: IUpdateRecord[];
 }
 
+/**
+ * This is documentation for interface IUpdateOptions.
+ *  @property tokens This is the description for the tokens property
+ */
 export interface IUpdateOptions{
   tokens: boolean
 }
