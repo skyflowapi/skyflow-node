8758a943696830555028cd43a33688cfe02c7a6f
“amith-skyflow” <amit@skyflow.com>
2024-11-21 23:00:50 +0530

    SK-1621: add test

--- src/utils/jwt-utils/index.ts
+++ src/utils/jwt-utils/index.ts
@@ -24,15 +24,15 @@ function isExpired(token: string) {
 
 function isTokenValid(token: string) {
     try {
-    if (token === "") return false
-    let isJwtExpired = false;
-    const decoded: JwtPayload = jwt_decode(token);
-    const currentTime = (new Date().getTime() / 1000);
-    const expiryTime = decoded.exp;
-    if (expiryTime && currentTime > expiryTime) {
-        isJwtExpired = true;
-    }
-    return !isJwtExpired;
+        if (token === "") return false
+        let isJwtExpired = false;
+        const decoded: JwtPayload = jwt_decode(token);
+        const currentTime = (new Date().getTime() / 1000);
+        const expiryTime = decoded.exp;
+        if (expiryTime && currentTime > expiryTime) {
+            isJwtExpired = true;
+        }
+        return !isJwtExpired;
     } catch (err) {
         return false;
     }
