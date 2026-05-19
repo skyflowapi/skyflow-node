1dfff9d84cba4cc0f72c745b2df70f7d29ad044f
Aadarsh <aadarsh.thakur@skyflow.com>
2026-05-19 17:28:28 +0530

    SK-2812: Updated contracts

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,13 +1,12 @@
 import { BAD_REQUEST, ISkyflowError, LogLevel, MessageType, parameterizedString, printLog } from "../utils";
-import logs from '../utils/logs';
 
 class SkyflowError extends Error {
 
     error?: ISkyflowError;
 
     constructor(errorCode: ISkyflowError, args: Array<string | number> = []) {
-        const formattedError: ISkyflowError = {
-            http_status: errorCode.http_status ?? BAD_REQUEST,
+        const formattedError: any = {
+            http_status: errorCode.http_status || BAD_REQUEST,
             details: errorCode.details || [],
             requestId: errorCode.requestId || null,
             grpc_code: errorCode.grpc_code || null,
